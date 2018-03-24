92b48dea4cf2796e1a077a0565b281ad93d656b5
navigonic <navidks@kth.se>
2018-03-24 18:31:38 +0100

    Update pizza.py

--- pizza.py
+++ pizza.py
@@ -1,6 +1,5 @@
-# Orginalrecept och kod av Pokemonkejsarn https://github.com/pokemonkejsaren/c_recipes/blob/master/pizza.c
-# Pythonifierad
-
+# Orginalrecept och kod av Pokemonkejsaren https://github.com/pokemonkejsaren/c_recipes/blob/master/pizza.c
+# Pythonifierad med förbättringar avseende föregångarens vägrande att använda torrjäst
 # Basala vikten i gram för en pizza, ca 170 g normalstor
 base_weight = 170
 
@@ -16,15 +15,16 @@ except ValueError:
         
 weight = base_weight * nbr_eaters
 
-# Proportionerna relativt till vetemjölen * totala degmassan för antalet pizzor
+#Proportionerna * totala degmassan för antalet pizzor
 flour = 1.0*weight
 water = 0.610*weight
 oil = 0.075*weight
 yeast = 0.005*weight
+dryyeast = round(((14/50)*yeast),2)
 salt = 0.02*weight
 
 print("Ingredienser:")
-print(' - Vetemjöl: {0}g \n - Vatten: {1}g \n - Olja: {2}g \n - Jäst: {3}g \n - Salt: {4}g'.format(flour, water, oil, yeast, salt))
+print(' - Vetemjöl: {}g \n - Vatten: {}g \n - Olja: {}g \n - Jäst: {}g / Torrjäst: {}g \n - Salt: {}g'.format(flour, water, oil, yeast, dryyeast, salt))
 print("Instruktioner:")
 print("Lös upp jästen, lägg till alla ingredienser, knåda...KNÅDA")
 print("låt jäsa åtminstone 18h i kyl lmao")
